879d55eb5ad9bd876dffdc835a989ee201431c4e
akp28 <ajaypaladugu@gmail.com>
2018-12-18 19:11:07 -0800

    intial commit with front end

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+      name: "test",
+      photo: "test.png",
+      scores:[
+          "5",
+          "4",
+          "3",
+          "2",
+          "1",
+          "5",
+          "4",
+          "3",
+          "2",
+          "1"
+      ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
